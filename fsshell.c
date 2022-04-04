bc6faf84d2f61589ded19eac30ef472a2d2b63da
Student <kkistenbroker@mail.sfsu.edu>
2022-04-04 15:14:25 -0700

    updated headers

--- fsshell.c
+++ fsshell.c
@@ -1,9 +1,15 @@
 /**************************************************************
 * Class:  CSC-415-0# - Fall 2021
-* Names: 
-* Student IDs:
-* GitHub Name:
-* Group Name:
+* 
+* Names: Kilian Kistenbroker, Emily Huang, Sean Locklar, 
+* Shauhin Pourshayegan
+*
+* Student IDs: 920723372, 920499746, 920506337, 920447681
+* 
+* GitHub Name: KilianKistenbroker
+* 
+* Group Name: Team Poke
+*
 * Project: Basic File System
 *
 * File: fsShell.c
