44a8e13c1006efc0d126d49b72895bfb7a7cf14a
Student <kkistenbroker@mail.sfsu.edu>
2022-04-09 11:51:52 -0700

    reorganized

--- dir_func.c
+++ dir_func.c
@@ -6,12 +6,80 @@
 #include <math.h>
 #include <stdint.h>
 #include <mfs.h>
+#include "freeAlloc.c"
+/*
+example of make dir with permissions
+status = mkdir("/home/cnd/mod1", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+*/
 
-int fs_mkdir(const char *pathname, mode_t mode) {
+dir_entr * dirent;
+void create_dir(char * name, char * parentName, int parent_starting_block, int permissions) 
+{
+    if (dirent == NULL)
+    {
+        dirent = malloc(VCB->block_size*6);
+    }
+    
+    strncpy(dirent[0].filename, name, sizeof(name));
+	strncpy(dirent[1].filename, parentName, sizeof(parentName));
+
+    dirent[0].size = dirent[1].size = sizeof(dir_entr) * 64;
+    dirent[0].permissions = dirent[1].permissions = permissions;
+	dirent[0].is_file = dirent[1].is_file = 0;
+
+    dirent[1].starting_block = parent_starting_block;
+    VCB->root_start = dirent[0].starting_block = allocate_space(5);
+
+    printf("------------CREATED NEW DIRECTORY------------\n");
+    printf("Size of new dir: %d \n", dirent[0].size);
+	printf("dir[0].starting_block : %d \n", dirent[0].starting_block);
+    printf("dir[1].starting_block : %d \n", dirent[1].starting_block);
+	printf("dir[0].filename : %s \n", dirent[0].filename);
+	printf("dir[1].filename : %s \n\n", dirent[1].filename);
+
+
+    for (int i = 2; i < 64; i++)
+	{
+		// empty filename will imply that it is free to write to
+		strncpy(dirent[i].filename, "", 0);
+	}
+
+    LBAwrite(dirent, 6, dirent[0].starting_block);
+
+	LBAwrite(VCB, 1, 0);
+}
+
+// to be called in mk_dir()
+int find_dir(char * pathnames, int starting_block, int counter) {
+    if (dirent == NULL)
+    {
+        dirent = malloc(VCB->block_size*6);
+    }
+
+    LBAread(dirent, 6, starting_block);
+    int my_count = counter++;
+
 
 }
 
-int fs_rmdir(const char *pathname) {
+char * parse_path(char *pathname) 
+{
+
+}
+
+int fs_mkdir(const char *pathname, mode_t mode) 
+{
+    // will return -1  if invalid, and 0 if success
+
+    // call parse path to divide string into array of searchable strings
+
+    // call find dir path. this will return a value depending on success or failure
+
+    //call create_dir if success else return -1
+}
+
+int fs_rmdir(const char *pathname) 
+{
 
 }
 
--- fsInit.c
+++ fsInit.c
@@ -31,7 +31,7 @@
 
 #include "fsLow.h"
 #include "mfs.h"
-#include "freeAlloc.c"
+#include "dir_func.c"
 
 /*
 This function is used for cleaning the blocks.
@@ -67,80 +67,23 @@ int initFileSystem (uint64_t numberOfBlocks, uint64_t blockSize)
 	//VCB is updated with whatever is at position 0
 	LBAread(VCB, 1, 0);
 
-	/*
-	checking if magic number of block 0 is 3.
-	if it is 3 then VCB is already formatted,
-	and we don't need to initialize.
-	*/
-
+	
+	//checking if magic number is a match
 	if (VCB->magic_num != 3)
 	{
 		flush_blocks(numberOfBlocks, blockSize);
+
 		VCB->magic_num = 3;
 		VCB->total_blocks = numberOfBlocks;
 		VCB->block_size = blockSize;
-
-		//init_bitmap() also returns the first free block in freespace bitmap
+		
 		init_bitmap(numberOfBlocks, blockSize); 
 		update_free_block_start(blockSize);
 
 		// --------- INIT ROOT DIRECTORY ---------- //
 
-		/*
-		creating an array of 51 dir_entries
-		changed sizeof(dir_entr) -> sizeof(root_dir) * 5
-		since sizeof(dir_entr) was producing weird hexdump error
-		*/
-
-		dir_entr * root_dir = malloc(blockSize * 6); 
-			
-		// printf("Size of dir_entr: %ld \n", sizeof(dir_entr));
-		// printf("blocks to allocate for root: %ld \n\n", (sizeof(dir_entr)* 64) / 512);
-
-		strncpy(root_dir[0].filename, ".", 1);
-		strncpy(root_dir[1].filename, "..", 2);
-
-		root_dir[0].size = root_dir[1].size = sizeof(dir_entr) * 64;
-
-		root_dir[0].permissions = root_dir[1].permissions = 700;
-
-		root_dir[0].is_file = root_dir[1].is_file = 0;
-
-		/*
-		I want to allocate 6 blocks for the root. allocate_space() 
-		will return the starting block in the freespace bitmap.
-		VCB->dir_entr_start will represent the starting block of
-		the root directory.
-		*/
-
-		VCB->dir_entr_start = root_dir[0].starting_block = allocate_space(5, numberOfBlocks);
-		
-		root_dir[1].starting_block = root_dir[0].starting_block;
-		
-
-		// printf("Size of root_dir: %d \n", root_dir[0].size);
-		// printf("root_dir[0].starting_block : %d \n", root_dir[0].starting_block);
-		// printf("root_dir[0].filename : %s \n", root_dir[0].filename);
-		// printf("root_dir[1].filename : %s \n\n", root_dir[1].filename);
-
-		for (int i = 2; i < 64; i++)
-		{
-			// empty filename will imply that it is free to write to
-			strncpy(root_dir[i].filename, "", 0);
-		}
-
-		LBAwrite(root_dir, 6, root_dir[0].starting_block);
-
-		LBAwrite(VCB, 1, 0);
-
-		//---------- cleaning up malloc'd spaces ---------- //
-
-		free(root_dir);
-		root_dir = NULL;
+		create_dir(".", "..", VCB->free_block_start, 700);
 	}
-
-		free(VCB);
-		VCB = NULL;
 		
 		return 0;
 }
@@ -148,6 +91,18 @@ int initFileSystem (uint64_t numberOfBlocks, uint64_t blockSize)
 	
 void exitFileSystem ()
 {
+	if (dirent != NULL)
+	{
+		free(dirent);
+		dirent = NULL;
+	}
+	
+	if (VCB != NULL)
+	{
+		free(VCB);
+		VCB = NULL;
+	}
+	
 	if (buffer_bitmap != NULL)
 	{
 		free(buffer_bitmap);
