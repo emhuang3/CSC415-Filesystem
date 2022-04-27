40e35c048d49190f18274dc46a0dc37764645b08
Student <kkistenbroker@mail.sfsu.edu>
2022-04-27 14:53:30 -0700

    updated shell

--- fsshell.c
+++ fsshell.c
@@ -1,5 +1,5 @@
 /**************************************************************
-* Class:  CSC-415-0# - Fall 2021
+* Class:  CSC-415-03 - Spring 2022
 * 
 * Names: Kilian Kistenbroker, Emily Huang, Sean Locklar, 
 * Shauhin Pourshayegan
